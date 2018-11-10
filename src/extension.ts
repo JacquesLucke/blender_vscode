61b7195904a8b857464a3b87dd30101368f7d63b
Jacques Lucke <mail@jlucke.com>
2018-11-10 11:47:56 +0100

    find path to Blender

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,75 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+var path = require('path');
+var fs = require('fs');
+const { exec } = require('child_process');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "blender-development" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let disposable = vscode.commands.registerCommand('b3ddev.startBlender', () => {
+        findAndUpdateBlenderPath(blenderPath => {
+            exec(blenderPath);
+        });
     });
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+function getConfiguration() {
+    return vscode.workspace.getConfiguration('b3ddev')
+}
+
+function getConfigBlenderPath() {
+    return getConfiguration().get('blenderPath');
+}
+
+function setConfigBlenderPath(path : string) {
+    getConfiguration().update('blenderPath', path);
+}
+
+function testIfPathIsBlender(filepath : string, callback : (isValid : boolean) => void) {
+    let name : string = path.basename(filepath);
+
+    if (name.toLowerCase().startsWith('blender')) {
+        let testString = '###TEST_BLENDER###';
+        exec(`${filepath} -b --python-expr "import sys;print('${testString}');sys.stdout.flush();sys.exit()"`, {},
+            (error : Error, stdout : string | Buffer, stderr : string | Buffer) => {
+                let text = stdout.toString();
+                callback(text.includes(testString));
+            });
+    } else {
+        callback(false);
+    }
+}
+
+function findAndUpdateBlenderPath(whenFound : (path : string) => void) {
+    let originalPath = getConfigBlenderPath();
+    fs.stat(originalPath, (err: Error, stat: any) => {
+        if (err === null && typeof originalPath === 'string') {
+            whenFound(originalPath);
+        } else {
+            vscode.window.showOpenDialog({
+                canSelectFiles: true,
+                canSelectFolders: false,
+                canSelectMany: false,
+                openLabel: 'Blender Executable'
+            }).then(value => {
+                if (value !== undefined) {
+                    let filepath = value[0].path;
+                    testIfPathIsBlender(filepath, is_valid => {
+                        if (is_valid) {
+                            setConfigBlenderPath(filepath);
+                            whenFound(filepath);
+                        } else {
+                            vscode.window.showErrorMessage('Not a valid Blender executable.');
+                        }
+                    });
+                }
+            });
+
+        }
+    });
 }
