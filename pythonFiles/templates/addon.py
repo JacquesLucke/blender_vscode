e744e3d790d37e162117218b51a4d05550e5fca0
Jacques Lucke <mail@jlucke.com>
2018-11-20 17:03:26 +0100

    better default addon template

--- pythonFiles/templates/addon.py
+++ pythonFiles/templates/addon.py
@@ -21,8 +21,16 @@ bl_info = {
     "category" : "Generic"
 }
 
+import bpy
+
+classes = (
+
+)
+
 def register():
-    ...
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
 def unregister():
-    ...
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
