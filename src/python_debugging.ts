76f3d3e92a09981025fde132044276f28517f079
Jacques Lucke <mail@jlucke.com>
2018-11-22 15:14:27 +0100

    cleanup

--- src/python_debugging.ts
+++ src/python_debugging.ts
@@ -1,14 +1,16 @@
-import * as vscode from 'vscode';
 import * as path from 'path';
+import * as vscode from 'vscode';
 import { AddonFolder } from './addon_folder';
 import { BlenderFolder } from './blender_folder';
 
-export async function attachPythonDebuggerToBlender(port : number, blenderPath : string, scriptsFolder : string) {
+export async function attachPythonDebuggerToBlender(
+    port : number, blenderPath : string, scriptsFolder : string)
+{
     let mappings = await getPythonPathMappings(blenderPath, scriptsFolder);
     attachPythonDebugger(port, mappings);
 }
 
-export function attachPythonDebugger(port : number, pathMappings : {localRoot:string, remoteRoot:string}[] = []) {
+function attachPythonDebugger(port : number, pathMappings : {localRoot:string, remoteRoot:string}[] = []) {
     let configuration = {
         name: `Python at Port ${port}`,
         request: "attach",
@@ -28,7 +30,7 @@ async function getPythonPathMappings(blenderPath : string, scriptsFolder : strin
             remoteRoot: addon.getLoadDirectory(),
         });
     }
-    let blender = await BlenderFolder.Get()
+    let blender = await BlenderFolder.Get();
     if (blender !== null) {
         mappings.push({
             localRoot: path.join(blender.uri.fsPath, 'release', 'scripts'),
