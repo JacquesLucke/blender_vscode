6ad12d0a1092dab2b165e6029087ee63734cdd0a
Jacques Lucke <mail@jlucke.com>
2018-11-22 15:06:10 +0100

    remove dead code

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,22 +1,6 @@
-import * as path from 'path';
 import * as vscode from 'vscode';
-import * as generic from './generic';
 
 
 export function getConfiguration(resource : vscode.Uri | undefined = undefined) {
     return vscode.workspace.getConfiguration('blender', resource);
-}
-
-export async function folderIsBlender(folder : vscode.WorkspaceFolder) {
-    let paths = ['doc', 'source', 'release'].map(n => path.join(folder.uri.fsPath, n));
-    return generic.pathsExist(paths);
-}
-
-export async function getBlenderWorkspaceFolder() {
-    for (let folder of generic.getWorkspaceFolders()) {
-        if (await folderIsBlender(folder)) {
-            return folder;
-        }
-    }
-    return null;
 }
