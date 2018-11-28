307611feb1417799e089050f7933e423614731bd
Jacques Lucke <mail@jlucke.com>
2018-11-27 17:28:38 -0800

    better error handling

--- pythonFiles/launch.py
+++ pythonFiles/launch.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import traceback
 from pathlib import Path
 
 include_dir = Path(__file__).parent / "include"
@@ -8,8 +9,12 @@ sys.path.append(str(include_dir))
 
 import blender_vscode
 
+try:
 blender_vscode.startup(
     editor_address=f"http://localhost:{os.environ['EDITOR_PORT']}",
     addon_paths=json.loads(os.environ['ADDON_DIRECTORIES_TO_LOAD']),
     allow_modify_external_python=os.environ['ALLOW_MODIFY_EXTERNAL_PYTHON'] == "yes",
 )
+except:
+    traceback.print_exc()
+    sys.exit()
