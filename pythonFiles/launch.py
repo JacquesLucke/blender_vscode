030a06ddf617b050cd6cf4263b5a9fd9c1d4e8db
Jacques Lucke <mail@jlucke.com>
2018-11-27 17:32:54 -0800

    fix

--- pythonFiles/launch.py
+++ pythonFiles/launch.py
@@ -10,11 +10,11 @@ sys.path.append(str(include_dir))
 import blender_vscode
 
 try:
-blender_vscode.startup(
-    editor_address=f"http://localhost:{os.environ['EDITOR_PORT']}",
-    addon_paths=json.loads(os.environ['ADDON_DIRECTORIES_TO_LOAD']),
-    allow_modify_external_python=os.environ['ALLOW_MODIFY_EXTERNAL_PYTHON'] == "yes",
-)
+    blender_vscode.startup(
+        editor_address=f"http://localhost:{os.environ['EDITOR_PORT']}",
+        addon_paths=json.loads(os.environ['ADDON_DIRECTORIES_TO_LOAD']),
+        allow_modify_external_python=os.environ['ALLOW_MODIFY_EXTERNAL_PYTHON'] == "yes",
+    )
 except:
     traceback.print_exc()
     sys.exit()
