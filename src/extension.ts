494bf122f5ace93c497964fa6f3490423b5f242c
Jacques Lucke <mail@jlucke.com>
2020-08-22 11:15:35 +0200

    initial server in vscode

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,22 @@
-'use strict';
-
 import * as vscode from 'vscode';
+import { ensureServer, stopServer } from './server';
 
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Hello World');
+    let port = ensureServer();
+    console.log('Port: ' + port);
+
+    const commands: [string, () => Promise<void>][] = [
+        ['blender.connect', COMMAND_connect],
+    ];
+
+    for (const [identifier, func] of commands) {
+        context.subscriptions.push(vscode.commands.registerCommand(identifier, func));
+    }
 }
 
 export function deactivate() {
+    stopServer();
 }
+
+async function COMMAND_connect() { }
