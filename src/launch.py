9fa8741432ded19494f12cbac75932f43c76064e
Jacques Lucke <mail@jlucke.com>
2020-08-23 14:28:16 +0200

    improve automatic installation of pip and ptvsd

--- src/launch.py
+++ src/launch.py
@@ -30,15 +30,23 @@ blender_vscode_addon.communication.set_vscode_address(vscode_address)
 blender_vscode_addon.communication.ensure_server_is_running()
 blender_vscode_addon.preferences.send_connection_info()
 
+from blender_vscode_addon.package_installation import is_package_installed, blender_uses_own_python
+
 if want_to_attach_python_debugger:
-    if not blender_vscode_addon.package_installation.is_package_installed("ptvsd"):
-        print("Error: Please install ptvsd before trying to attach the debugger.")
-        sys.exit(1)
-    else:
-        bpy.ops.development.start_ptvsd_server()
-        blender_vscode_addon.preferences.send_connection_info()
-        import ptvsd
-        blender_vscode_addon.communication.send_command("/attach_ptvsd")
-        print("Waiting for Python debugger to attach.")
-        ptvsd.wait_for_attach()
-        print("Attached.")
+    if not is_package_installed("ptvsd"):
+        if blender_uses_own_python():
+            if not is_package_installed("pip"):
+                bpy.ops.development.install_pip()
+            bpy.ops.development.install_python_package(package_name="ptvsd")
+        else:
+            print("Error: Please install ptvsd before trying to attach the debugger.")
+            print("Did not try to install ptvsd automatically, because Blender does not use its own Python.")
+            sys.exit(1)
+
+    bpy.ops.development.start_ptvsd_server()
+    blender_vscode_addon.preferences.send_connection_info()
+    import ptvsd
+    blender_vscode_addon.communication.send_command("/attach_ptvsd")
+    print("Waiting for Python debugger to attach.")
+    ptvsd.wait_for_attach()
+    print("Attached.")
