578db026badad36d87a1e1c40612ef962e5f12be
Jacques Lucke <mail@jlucke.com>
2020-08-23 14:44:10 +0200

    fix

--- src/launch.py
+++ src/launch.py
@@ -17,7 +17,7 @@ link_addon_path = addons_dir / addon_name
 if not link_addon_path.exists():
     if sys.platform == "win32":
         import _winapi
-        _winapi.CreateJunction(str(real_addon_path), link_addon_path)
+        _winapi.CreateJunction(str(real_addon_path), str(link_addon_path))
     else:
         os.symlink(str(real_addon_path), str(link_addon_path), target_is_directory=True)
 
